38faa6c68393ea3a7d36a2af545a94b2108303cc
INNAKIPER <kiperinna4@gmail.com>
2024-10-13 22:03:51 +0300

    Домашняя работа по уроку "Стиль кода часть II. Цикл While"

---
+++ module 2.3.py
@@ -0,0 +1,18 @@
+
+
+
+#animal=['cat','dog','bat']
+#i=0
+#while i <len(animal):
+#    print(animal[i])
+#    i+=1
+
+my_list=[42,69,322,13,0,99,-5,9,8,7,-6,5]
+nol=0
+while nol<len(my_list):
+    if my_list[nol]>0:
+        print(my_list[nol])
+    elif my_list[nol]<0:
+         break
+    nol+=1
+
